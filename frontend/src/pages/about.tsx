1437740c94cc581bf518fbf436bfdda32c70af65
Loys <louisetalip@gmail.com>
2021-05-28 14:31:38 +0800

    chore: fixed photo in about page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -11,7 +11,7 @@ import {
 import { NavigationBar } from "../components/NavigationBar";
 import { Footer } from "../components/Footer";
 import kuleng_avatar from "./../assets/jkbtumpalan_avatar.jpg";
-import loys_avatar from "./../assets/lgltalip_avatar.jpg";
+import loys_avatar from "./../assets/lgltalip_avatar.JPG";
 
 const testimonials = [
   {
