d999c2c63748f3ad74158ecfdc5be45daee941b7
Loys <louisetalip@gmail.com>
2021-06-04 14:06:17 +0800

    fix: removed changes to appointment

--- frontend/src/pages/appointment.tsx
+++ frontend/src/pages/appointment.tsx
@@ -19,13 +19,19 @@ import {
   Center,
   Stack,
   Select,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
 } from "@chakra-ui/react";
 import { SlideFade } from "@chakra-ui/react";
 import { Footer } from "../components/Footer";
 import { Calendar } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import { GiConsoleController } from "react-icons/gi";
 
 const vaccination_sites: {
   name: string;
@@ -48,12 +54,41 @@ const vaccination_sites: {
 ];
 
 export default function Appointment() {
-  const [date, setDate] = useState(null)
-  // const 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [date, setDate] = useState(null);
 
-  const handleSubmit = (evt) => {
-    console.log(evt.target.value)
+  const datenow = new Date();
+
+  //Create random disabled dates.
+  function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max) + 2;
+  }
+
+  const createDate = (offset) => {
+    return new Date(
+      datenow.getFullYear(),
+      datenow.getMonth(),
+      datenow.getDate() + getRandomInt(offset)
+    );
   };
+
+  const dt = new Date();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  const generateDisabledDates = () => {
+    return [
+      createDate(daysInMonth * 2 - datenow.getDate()),
+      createDate(daysInMonth * 2 - datenow.getDate()),
+      createDate(daysInMonth * 2 - datenow.getDate()),
+      createDate(daysInMonth * 2 - datenow.getDate()),
+    ];
+  };
+
+  componentDidUpdate() {
+    
+  }
 
   return (
     <>
@@ -97,61 +132,80 @@ export default function Appointment() {
                 <br />
                 <br />
               </Heading>
-              <form onSubmit={handleSubmit}>
-                <FormControl isRequired>
-                  <FormLabel as="legend">Vaccination Facility</FormLabel>
-                  <Select
-                    variant="outline"
-                    border="1px"
-                    borderColor="twitter.900"
-                  >
-                    {vaccination_sites.map((site, key) => {
-                      return (
-                        <option
-                          key={key}
-                          value={site.name.toString().toLowerCase()}
-                        >
-                          {site.name}
-                        </option>
-                      );
-                    })}
-                  </Select>
-                </FormControl>
-                <FormControl isRequired>
-                  <FormLabel as="legend">Choose Appointment Date</FormLabel>
-                  <Calendar
-                    date={date}
-                    minDate={new Date()}
-                    onChange={(item) => setDate(item)}
-                    color="#009183"
-                    supressHydrationWarning
-                  />
-                  <FormHelperText color="gray.700">
-                    Note: The appointment you have chosen is not a guarantee that
-                    you will be given that slot. The request will still be
-                    processed and reviewed by the medical workers administering
-                    the vaccines.
-                  </FormHelperText>
-                </FormControl>
-                <FormControl>
-                  <FormLabel as="legend">Other Information</FormLabel>
-                  <Textarea
-                    variant="outline"
-                    size="md"
-                    border="1px"
-                    borderColor="twitter.900"
-                  />
-                </FormControl>
-                <Button
-                  colorScheme="green.300"
-                  size="md"
-                  width="30%"
-                  aria-label="Submit"
-                  type='submit'
+              <FormControl isRequired>
+                <FormLabel as="legend">Vaccination Facility</FormLabel>
+                <Select
+                  variant="outline"
+                  border="1px"
+                  borderColor="twitter.900"
+                  isRequired
                 >
-                  Submit
-                </Button>
-              </form>
+                  {vaccination_sites.map((site, key) => {
+                    return (
+                      <option
+                        key={key}
+                        value={site.name.toString().toLowerCase()}
+                      >
+                        {site.name}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel as="legend">Choose Appointment Date</FormLabel>
+                <Calendar
+                  date={date}
+                  minDate={new Date()}
+                  disabledDates={generateDisabledDates()}
+                  onChange={(item) => setDate(item)}
+                  color="#009183"
+                  supressHydrationWarning
+                  isRequired
+                />
+                <FormHelperText color="gray.700">
+                  Note: The appointment you have chosen is not a guarantee that
+                  you will be given that slot. The request will still be
+                  processed and reviewed by the medical workers administering
+                  the vaccines.
+                </FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel as="legend">Other Information</FormLabel>
+                <Textarea
+                  variant="outline"
+                  size="md"
+                  border="1px"
+                  borderColor="twitter.900"
+                />
+              </FormControl>
+              <Button
+                colorScheme="green"
+                size="md"
+                width="30%"
+                aria-label="Submit"
+                onClick={onOpen}
+              >
+                Submit
+              </Button>
+
+              <Modal isOpen={isOpen} onClose={onClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>Confirm Request</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody>
+                      <Text>
+                        Hello there you
+                      </Text>
+                    </ModalBody>
+
+                    <ModalFooter>
+                      <Button variant="ghost">Back</Button>
+                      <Button colorScheme="green" mr={3} onClick={onClose}>Confirm</Button>
+                    </ModalFooter>
+                  </ModalContent>
+              </Modal>
             </Stack>
           </Box>
         </Center>
